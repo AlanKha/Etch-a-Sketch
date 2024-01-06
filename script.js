517f0c73b071aeb9b5f7ac0998504b68a02661ab
AlanKha <khalilialan@gmail.com>
2024-01-05 22:54:00 -0800

    work in progress 1/5/24

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function updateGrid(size){
+        gridContainer.innerHTML = '';
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        for(let i = 0; i < size * size; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridContainer.appendChild(gridItem);
+        }
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    }
+
+    const defaultSize = 8;
+    const gridContainer = document.querySelector('.grid-container');
+    const gridSizeInput = document.getElementById('grid-size');
+
+    updateGrid(defaultSize);
+
+    gridSizeInput.addEventListener('input', function () {
+        updateGrid(gridSizeInput.value);
+    });
+});
