09c373b0b8d52b79567f6a9286a814d8f0813e5f
AlanKha <khalilialan@gmail.com>
2024-01-07 20:46:05 -0800

    Eraser and Rainbow Buttons working

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    const defaultSize = 16;
+    const gridContainer = document.querySelector('.grid-container');
+    const gridSizeInput = document.getElementById('grid-size');
+    const clearButton = document.getElementById('clear');
+    const gridSizeDisplay = document.getElementById('grid-size-display');
+    const blackBtn = document.querySelector('#black');
+    const rainbowBtn = document.querySelector('#rainbow');
+    const eraserBtn = document.querySelector('#eraser')
+
+    let black = true;
+    let rainbow = false;
+    let eraser = false;
+
+    blackBtn.onclick = () => {
+        black = true;
+        rainbow = false;
+        eraser = false;
+    }
+    rainbowBtn.onclick = () =>{
+        black = false;
+        rainbow = true;
+        eraser = false;
+    }
+    eraserBtn.onclick = () => {
+        black = false;
+        rainbow = false;
+        eraser = true;
+    }
+
+    function clearGrid() {
+        const gridItems = document.querySelectorAll('.grid-item');
+        gridItems.forEach(gridItem => {
+            gridItem.style.backgroundColor = 'white';
+        });
+    }
+
     function updateGrid(size){
         gridContainer.innerHTML = '';
+        gridSizeDisplay.textContent = `${size}x${size}`
         gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         for(let i = 0; i < size * size; i++) {
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
 
             gridItem.addEventListener('mouseover', function () {
-                gridItem.style.backgroundColor = 'black';
+                if(black){
+                    gridItem.style.backgroundColor = 'black';
+                }
+                if(eraser){
+                    gridItem.style.backgroundColor = 'white';
+                }
+                if(rainbow){
+                    gridItem.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                }
             });
 
             gridContainer.appendChild(gridItem);
@@ -16,9 +62,9 @@ document.addEventListener('DOMContentLoaded', function () {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     }
 
-    const defaultSize = 8;
-    const gridContainer = document.querySelector('.grid-container');
-    const gridSizeInput = document.getElementById('grid-size');
+    
+
+    clearButton.onclick = () => clearGrid();
 
     updateGrid(defaultSize);
 
